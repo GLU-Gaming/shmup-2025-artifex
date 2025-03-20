bcd1575140bb24c46f5631746c45b7dec2f9a391
TomMichaux <tommiemichaux@gmail.com>
2025-03-20 11:46:07 +0100

    Create Light Enemy Mech.cs

---
+++ Assets/Developers/Tom's Assets/Scripts/Light Enemy Mech.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class LightEnemyMech : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+
+    bool TargetDownOrUp = true;
+
+    [SerializeField] private float TargetspeedIncreaseY;
+
+    private float TargetspeedY;
+
+    [SerializeField] private float AttackCoolDown = 5f;
+
+    public float minimum = -1.0F;
+    public float maximum = 1.0F;
+
+
+    static float t = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        AttackCoolDown -= Time.deltaTime;
+
+        TargetspeedY = TargetspeedIncreaseY;
+
+        if (transform.position.y >= 10)
+        { if(AttackCoolDown >= 0)
+            {
+                TargetDownOrUp = false;
+            }
+            
+        }
+        else if (transform.position.y <= -10)
+        {
+            if (AttackCoolDown >= 0)
+            {
+                TargetDownOrUp = true;
+            }
+            
+        }
+        if (TargetDownOrUp == false)
+        {
+            if (AttackCoolDown >= 0)
+            {
+                TargetspeedIncreaseY = -0.30f;
+            }
+            
+        }
+
+        if (TargetDownOrUp == true)
+        {
+            if (AttackCoolDown >= 0)
+            {
+                TargetspeedIncreaseY = 0.30f;
+            }
+
+        }
+        if (AttackCoolDown >= 0)
+        {
+            transform.position += new Vector3(0, TargetspeedY * Time.deltaTime * 20f, 0);
+        }
+        if(AttackCoolDown < 0)
+        {
+            AttackCoolDown = -0.1f;
+            transform.position = new Vector3(transform.position.x - Time.deltaTime, Mathf.Lerp(Player.position.y, maximum, t), transform.position.z);
+
+            t += 0.5f * Time.deltaTime;
+
+            if (t > 1f)
+            {
+                float temp = maximum;
+                maximum = minimum;
+                minimum = temp;
+                t = 0.0f;
+            }
+
+        }
+       
+
+    }
+}
+
