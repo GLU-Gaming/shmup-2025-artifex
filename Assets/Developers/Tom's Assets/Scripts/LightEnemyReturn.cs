aa14aa62d4e2c7c4ed95d78271bdcacde8fe168d
TomMichaux <tommiemichaux@gmail.com>
2025-03-21 11:08:42 +0100

    Tom.ReturnEnemyUpdate02

--- Assets/Developers/Tom's Assets/Scripts/LightEnemyReturn.cs
+++ Assets/Developers/Tom's Assets/Scripts/LightEnemyReturn.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class LightEnemyReturn : MonoBehaviour
 {
-    
+    [SerializeField] private Transform LightEnemy;
+
+
+    private LightEnemyMech LightEnemymech;
     void Start()
     {
-        
+        LightEnemymech = FindFirstObjectByType<LightEnemyMech>();
     }
 
     
     void Update()
     {
-        
+        if(LightEnemymech.AttackCoolDown >= 0)
+        {
+            transform.position = new Vector3(LightEnemy.transform.position.x, LightEnemy.transform.position.y, LightEnemy.transform.position.z);
+        }
     }
 }
